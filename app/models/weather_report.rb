625fa2d824f0606bbb57c944bdfdb27441c7f539
ScottKohler <scottdkohler@gmail.com>
2023-01-29 13:51:39 -0500

    recommitted controller and model to fix develop branch

--- app/models/weather_report.rb
+++ app/models/weather_report.rb
@@ -15,7 +15,9 @@ scope :show_cache, -> { where('? < (created_at + ?) ', DateTime.now.new_offset(0
 
   def self.find_by_zipcode zip_code
     p "you are in find_by_zipcode #{zip_code}"
-    self.where("zip_code = #{zip_code}").select(:id, :zip_code, :created_at, :updated_at, :response_text, :humidity, :temp_f, :wind_mph, :location_name)
+    self.where(WeatherReport.arel_table[:created_at].gt(30.minutes.ago))
+        .where("zip_code = #{zip_code}")
+        .select(:id, :zip_code, :created_at, :updated_at, :response_text, :humidity, :temp_f, :wind_mph, :location_name)
   end
 
 
